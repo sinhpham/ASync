03468c40a62b82786f4ed15c53fcfe65003f8999
Sinh Pham <phamansinh@gmail.com>
2013-03-17 00:02:16 -0600

    DataChunk file.

---
+++ ASync/DataChunk.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASync
+{
+    public class DataChunk<T>
+    {
+        const int BufferSize = 1024 * 4;
+
+        public T[] Data = new T[BufferSize];
+        public int DataSize { get; private set; }
+
+        public bool IsFull { get { return DataSize == BufferSize; } }
+
+        public void Add(T item)
+        {
+            Data[DataSize] = item;
+            ++DataSize;
+        }
+    }
+
+    public class BlockingCollectionDataChunk<T>
+    {
+        public BlockingCollectionDataChunk()
+        {
+            BlockingCollection = new BlockingCollection<DataChunk<T>>();
+            _currChunk = new DataChunk<T>();
+        }
+
+        public BlockingCollection<DataChunk<T>> BlockingCollection { get; private set; }
+        private DataChunk<T> _currChunk;
+
+        public void Add(T item)
+        {
+            if (!_currChunk.IsFull)
+            {
+                _currChunk.Add(item);
+                return;
+            }
+            // CurrChunk is full, need to create new one and add the current one to blocking collection
+            BlockingCollection.Add(_currChunk);
+            _currChunk = new DataChunk<T>();
+            _currChunk.Add(item);
+        }
+
+        public void CompleteAdding()
+        {
+            BlockingCollection.Add(_currChunk);
+            BlockingCollection.CompleteAdding();
+            _currChunk = null;
+        }
+    }
+}
--- ASync/Program.cs
+++ ASync/Program.cs
@@ -12,61 +12,13 @@ using System.Collections.Concurrent;
 
 namespace ASync
 {
-    public class DataChunk<T>
-    {
-        const int BufferSize = 2;
-
-        public T[] Data = new T[BufferSize];
-        public int DataSize { get; private set; }
-
-        public bool IsFull { get { return DataSize == BufferSize; } }
-
-        public void Add(T item)
-        {
-            Data[DataSize] = item;
-            ++DataSize;
-        }
-    }
-
-    public class BlockingCollectionDataChunk<T>
-    {
-        public BlockingCollectionDataChunk()
-        {
-            BlockingCollection = new BlockingCollection<DataChunk<T>>();
-            _currChunk = new DataChunk<T>();
-        }
-
-        public void Add(T item)
-        {
-            if (!_currChunk.IsFull)
-            {
-                _currChunk.Add(item);
-                return;
-            }
-            // CurrChunk is full, need to create new one and add the current one to blocking collection
-            BlockingCollection.Add(_currChunk);
-            _currChunk = new DataChunk<T>();
-            _currChunk.Add(item);
-        }
-
-        public void CompleteAdding()
-        {
-            BlockingCollection.Add(_currChunk);
-            BlockingCollection.CompleteAdding();
-            _currChunk = null;
-        }
-
-        public BlockingCollection<DataChunk<T>> BlockingCollection { get; private set; }
-        private DataChunk<T> _currChunk;
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            //ProcessFile("test.dat");
+            ProcessFile("test.dat");
 
-            LocalMaxima.StressTest();
+            //LocalMaxima.StressTest();
         }
 
         static void ProcessFile(string filename)
