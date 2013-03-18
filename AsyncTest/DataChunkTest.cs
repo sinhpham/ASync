fa3bcc80182d9a1dec505ed641a3b574e9983d17
Sinh Pham <phamansinh@gmail.com>
2013-03-18 10:22:21 -0600

    DataChunk test.

---
+++ AsyncTest/DataChunkTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ASync;
+using System.Collections.Generic;
+
+namespace AsyncTest
+{
+    [TestClass]
+    public class DataChunkTest
+    {
+        [TestMethod]
+        public void DataChunkTest1()
+        {
+            var b = new BlockingCollectionDataChunk<int>(3);
+            var oriList = new List<int>();
+            for (var i = 0; i < 100; ++i)
+            {
+                b.Add(i);
+                oriList.Add(i);
+            }
+            b.CompleteAdding();
+
+            var bl = b.ToList();
+
+            Assert.AreEqual(100, bl.Count);
+            CollectionAssert.AreEqual(oriList, bl);
+        }
+    }
+}
