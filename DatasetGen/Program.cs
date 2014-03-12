776715c603c799243dd43349a448db5fd12e5a27
Sinh Pham <phamansinh@gmail.com>
2014-03-11 23:54:26 -0600

    DatasetGen

---
+++ DatasetGen/Program.cs
@@ -0,0 +1,44 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatasetGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void GenDataSet(int baseSize, int changedPercent)
+        {
+            var clientDic = new Dictionary<string, string>();
+            for (var i = 0; i < baseSize; ++i)
+            {
+                clientDic.Add(i.ToString(), i.ToString());
+            }
+
+            var sSize = baseSize / 100 * changedPercent + baseSize;
+            var serverDic = new Dictionary<string, string>();
+            for (var i = 0; i < sSize; ++i)
+            {
+                serverDic.Add(i.ToString(), (i).ToString());
+            }
+
+            using (var f = File.Create("clientDic.dat"))
+            {
+                Serializer.Serialize(f, clientDic);
+            }
+
+            using (var f = File.Create("serverDic.dat"))
+            {
+                Serializer.Serialize(f, serverDic);
+            }
+        }
+    }
+}
