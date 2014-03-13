e20cfe11d620b060589085fb2a9de90480c2a271
Sinh Pham <phamansinh@gmail.com>
2014-03-13 17:17:39 -0600

    Datagen more efficient.

--- DatasetGen/Program.cs
+++ DatasetGen/Program.cs
@@ -12,32 +12,27 @@ namespace DatasetGen
     {
         static void Main(string[] args)
         {
-            GenDataSet(1000000, 50);
+            GenDataSet(10000000, 50);
         }
 
         static void GenDataSet(int baseSize, int changedPercent)
         {
-            var clientDic = new Dictionary<string, string>();
-            for (var i = 0; i < baseSize; ++i)
-            {
-                clientDic.Add(i.ToString(), i.ToString());
-            }
+            CreateDicFile(baseSize, string.Format("{0}-clientDic.dat", baseSize));
 
             var sSize = baseSize / 100 * changedPercent + baseSize;
-            var serverDic = new Dictionary<string, string>();
-            for (var i = 0; i < sSize; ++i)
-            {
-                serverDic.Add(i.ToString(), (i).ToString());
-            }
+            CreateDicFile(sSize, string.Format("{0}-{1}changed-serverDic.dat", sSize, changedPercent));
+        }
 
-            using (var f = File.Create(string.Format("{0}-clientDic.dat", baseSize)))
+        static void CreateDicFile(int size, string name)
+        {
+            var dic = new Dictionary<string, string>();
+            for (var i = 0; i < size; ++i)
             {
-                Serializer.Serialize(f, clientDic);
+                dic.Add(i.ToString(), i.ToString());
             }
-
-            using (var f = File.Create(string.Format("{0}-{1}changed-serverDic.dat", sSize, changedPercent)))
+            using (var f = File.Create(name))
             {
-                Serializer.Serialize(f, serverDic);
+                Serializer.Serialize(f, dic);
             }
         }
     }
