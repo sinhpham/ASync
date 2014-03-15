08ba6db4bbc2b566346b1da9d53cf8906c0967e1
Sinh Pham <phamansinh@gmail.com>
2014-03-15 15:40:25 -0600

    Add WP proj.

---
+++ ASyncWP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ASyncWP.Resources;
+
+namespace ASyncWP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
