3af34999f3c7282d9de12981dbb21c216008cc9d
Sinh Pham <phamansinh@gmail.com>
2014-03-15 15:27:26 -0600

    Remove CPI.

--- ASync/KeyValSync.cs
+++ ASync/KeyValSync.cs
@@ -13,8 +13,6 @@ namespace ASync
     {
         // Only use the last 24 bits
         const int HashBitMask = 0xFFFFFFF;
-        const int VerificationNum = 1;
-        const int FieldOrder = 2147483647;
 
         public static void ClientGenBfFile<TKey, TValue>(Dictionary<TKey, TValue> clientDic, string clientBFFile)
         {
@@ -75,102 +73,6 @@ namespace ASync
             using (var file = File.Create(patch1File))
             {
                 Serializer.Serialize(file, Tuple.Create(remainingD0, patchDic));
-            }
-        }
-
-        public static void ClientPatchAndGenCPFile<TKey, TValue>(Dictionary<TKey, TValue> clientDic, string patch1File, string cpFile)
-        {
-            var d0 = 0;
-            var patchDic = new Dictionary<TKey, TValue>();
-            using (var file = File.OpenRead(patch1File))
-            {
-                var t = Serializer.Deserialize<Tuple<int, Dictionary<TKey, TValue>>>(file);
-                d0 = t.Item1;
-                patchDic = t.Item2;
-            }
-            // Apply patch 1.
-            foreach (var item in patchDic)
-            {
-                clientDic[item.Key] = item.Value;
-            }
-
-            // Phase 2: using set reconciliation
-            var hFunc = new MurmurHash3_x86_32();
-            var _cp = new CharacteristicPolynomial(FieldOrder);
-            var xVal = GenXValues(FieldOrder, d0);
-
-            var clientSet = new List<int>();
-            foreach (var item in clientDic)
-            {
-                var block = item.Key + "-" + item.Value;
-                var bBlock = Helper.GetBytes(block);
-
-                var hv = hFunc.ComputeHash(bBlock);
-                var hIntValue = BitConverter.ToInt32(hv, 0) & HashBitMask;
-                clientSet.Add(hIntValue);
-            }
-
-            var cpClientSet = _cp.Calc(clientSet, xVal);
-
-            var cpdata = new CPData
-            {
-                CPValues = cpClientSet,
-                SetCount = clientDic.Count,
-            };
-
-            using (var file = File.Create(cpFile))
-            {
-                Serializer.Serialize(file, cpdata);
-            }
-        }
-
-        public static void ServerGenPatch2<TKey, TValue>(Dictionary<TKey, TValue> serverDic, string clientCPFile, string patch2File)
-        {
-            CPData cpd;
-            using (var file = File.OpenRead(clientCPFile))
-            {
-                cpd = Serializer.Deserialize<CPData>(file);
-            }
-
-            var hFunc = new MurmurHash3_x86_32();
-            var _cp = new CharacteristicPolynomial(FieldOrder);
-            var d0 = cpd.CPValues.Count;
-            var xVal = GenXValues(FieldOrder, d0);
-            var cpClientSet = cpd.CPValues;
-
-            var serverSet = new List<int>();
-            var hashToKey = new Dictionary<int, TKey>();
-            foreach (var item in serverDic)
-            {
-                var block = item.Key + "-" + item.Value;
-                var bBlock = Helper.GetBytes(block);
-
-                var hv = hFunc.ComputeHash(bBlock);
-                var hIntValue = BitConverter.ToInt32(hv, 0) & HashBitMask;
-                serverSet.Add(hIntValue);
-                hashToKey.Add(hIntValue, item.Key);
-            }
-            var cpServerSet = _cp.Calc(serverSet, xVal);
-
-            var cpaocpb = _cp.Div(cpClientSet, cpServerSet);
-
-            List<int> p;
-            List<int> q;
-            _cp.Interpolate(cpaocpb, xVal,
-                cpd.SetCount - serverDic.Count,
-                out p, out q);
-            var missingFromOldList = _cp.Factoring(q);
-
-            var patchDic = new Dictionary<TKey, TValue>();
-            foreach (var hValue in missingFromOldList)
-            {
-                var key = hashToKey[hValue];
-                patchDic[key] = serverDic[key];
-            }
-
-            using (var file = File.Create(patch2File))
-            {
-                Serializer.Serialize(file, patchDic);
             }
         }
 
@@ -269,113 +171,6 @@ namespace ASync
             }
         }
 
-        public static void SyncDic<TKey, TValue>(Dictionary<TKey, TValue> clientDic, Dictionary<TKey, TValue> serverDic)
-        {
-            var bf = new BloomFilter(clientDic.Count * 8, BloomFilter.DefaultHashFuncs());
-            var hFunc = new MurmurHash3_x86_32();
-
-            // Client side.
-            foreach (var item in clientDic)
-            {
-                var block = item.Key + "-" + item.Value;
-                var bBlock = Helper.GetBytes(block);
-
-                var hv = hFunc.ComputeHash(bBlock);
-
-                bf.Add(hv);
-            }
-
-            // Server side
-            var hitNum = 0;
-            var patchDic = new Dictionary<TKey, TValue>();
-            foreach (var item in serverDic)
-            {
-                var block = item.Key + "-" + item.Value;
-                var bBlock = Helper.GetBytes(block);
-
-                var hv = hFunc.ComputeHash(bBlock);
-                if (!bf.Contains(hv))
-                {
-                    patchDic.Add(item.Key, item.Value);
-                }
-                else
-                {
-                    hitNum++;
-                }
-            }
-
-            var estimatedDo = Helper.EstimateD0(clientDic.Count, serverDic.Count, hitNum, bf) + 20;
-            //var wrongNum = (int)Math.Ceiling(serverDic.Count * bf.FalsePositive);
-
-            var d0 = estimatedDo - patchDic.Count;
-
-            // Client side
-            foreach (var item in patchDic)
-            {
-                clientDic[item.Key] = item.Value;
-            }
-
-            SyncDicSetRecon(clientDic, serverDic, d0);
-        }
-
-        static void SyncDicSetRecon<TKey, TValue>(Dictionary<TKey, TValue> clientDic, Dictionary<TKey, TValue> serverDic, int d0)
-        {
-            var hFunc = new MurmurHash3_x86_32();
-            // Phase 2: using set reconciliation
-            var _cp = new CharacteristicPolynomial(FieldOrder);
-            var xVal = GenXValues(FieldOrder, d0);
-
-            var clientSet = new List<int>();
-            foreach (var item in clientDic)
-            {
-                var block = item.Key + "-" + item.Value;
-                var bBlock = Helper.GetBytes(block);
-
-                var hv = hFunc.ComputeHash(bBlock);
-                var hIntValue = BitConverter.ToInt32(hv, 0) & HashBitMask;
-                clientSet.Add(hIntValue);
-            }
-
-            var cpClientSet = _cp.Calc(clientSet, xVal);
-
-            // Server side
-            var serverSet = new List<int>();
-            var hashToKey = new Dictionary<int, TKey>();
-            foreach (var item in serverDic)
-            {
-                var block = item.Key + "-" + item.Value;
-                var bBlock = Helper.GetBytes(block);
-
-                var hv = hFunc.ComputeHash(bBlock);
-                var hIntValue = BitConverter.ToInt32(hv, 0) & HashBitMask;
-                serverSet.Add(hIntValue);
-                hashToKey.Add(hIntValue, item.Key);
-            }
-            var cpServerSet = _cp.Calc(serverSet, xVal);
-
-            var cpaocpb = _cp.Div(cpClientSet, cpServerSet);
-
-            List<int> p;
-            List<int> q;
-            _cp.Interpolate(cpaocpb, xVal,
-                clientDic.Count - serverDic.Count,
-                out p, out q);
-            var missingFromOldList = _cp.Factoring(q);
-
-            var patchDic = new Dictionary<TKey, TValue>();
-            foreach (var hValue in missingFromOldList)
-            {
-                var key = hashToKey[hValue];
-                patchDic[key] = serverDic[key];
-            }
-
-            // Client side
-            foreach (var item in patchDic)
-            {
-                clientDic[item.Key] = item.Value;
-            }
-        }
-
         public static bool AreTheSame<TKey, TValue>(Dictionary<TKey, TValue> dic1, Dictionary<TKey, TValue> dic2)
         {
             if (dic1.Count != dic2.Count)
@@ -395,19 +190,6 @@ namespace ASync
             }
 
             return true;
-        }
-
-        static List<int> GenXValues(int fieldOrder, int num)
-        {
-            var ret = new List<int>(num);
-            var addConst = fieldOrder - num;
-
-            for (var i = 0; i < num; ++i)
-            {
-                ret.Add(i + addConst);
-            }
-
-            return ret;
         }
     }
 
