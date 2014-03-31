2698f8aa4f7456cc5f578ad2a6aeb53542857fec
Sinh Pham <phamansinh@gmail.com>
2014-03-31 17:43:25 -0600

    Android client downloading.

--- ASyncAndroid/MainActivity.cs
+++ ASyncAndroid/MainActivity.cs
@@ -9,6 +9,7 @@ using ASyncLib;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ASyncAndroid
 {
@@ -28,22 +29,56 @@ namespace ASyncAndroid
             
             button.Click += async delegate
             {
-                //DbManager.CheckDataSet(5000000, 50);
+                //GenClientData(5000000);
+                //GenBfFileFromDb();
+                await DownloadPatch1File();
 
 
-                //using (var tran = DbManager.Engine.GetTransaction())
-                //{
-                //    //var clientDic = tran.SelectForward<string, string>("t1").Select(t => new KeyValuePair<string, string>(t.Key, t.Value));
-
-                //    var docFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
-                //    using (var bffile = File.OpenRead(Path.Combine(docFolder, "bffile.dat")))
-                //    {
-                //        await NetworkManager.FtpUpload("ftp://10.81.4.120", bffile, "bffile.dat");
-                //        //KeyValSync.ClientGenBfFile(clientDic, (int)tran.Count("t1"), bffile);
-                //    }
-                //}
                 var a = 0;
             };
+        }
+
+        private static void GenClientData(int size)
+        {
+            using (var trans = DbManager.Engine.GetTransaction())
+            {
+                foreach (var item in DataGen.Gen(size, 0))
+                {
+                    trans.Insert("t1", item.Key, item.Value);
+                }
+                trans.Commit();
+            }
+            DbManager.Dispose();
+        }
+
+        private static void GenBfFileFromDb()
+        {
+            using (var trans = DbManager.Engine.GetTransaction())
+            {
+                var clientDic = trans.SelectForward<string, string>("t1").Select(t => new KeyValuePair<string, string>(t.Key, t.Value));
+                var docFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+                using (var bffile = File.OpenWrite(Path.Combine(docFolder, "bffile.dat")))
+                {
+                    KeyValSync.ClientGenBfFile(clientDic, (int)trans.Count("t1"), bffile);
+                }
+            }
+            DbManager.Dispose();
+        }
+
+        private static async Task UploadBfFileToServer()
+        {
+            var docFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            using (var bffile = File.OpenRead(Path.Combine(docFolder, "bffile.dat")))
+            {
+                await NetworkManager.FtpUpload("ftp://10.81.4.120", bffile, "bffile.dat");
+            }
+        }
+
+        private static async Task DownloadPatch1File()
+        {
+            var docFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+           
+            await NetworkManager.FtpDownload("ftp://10.81.4.120/patch1file.dat", "patch1file.dat");
         }
     }
 }
