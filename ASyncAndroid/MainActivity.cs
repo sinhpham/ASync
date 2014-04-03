dc3a1534104d68185d02384640b5a169371a0af1
Sinh Pham <phamansinh@gmail.com>
2014-04-03 14:52:12 -0600

    Android client button for each function.

--- ASyncAndroid/MainActivity.cs
+++ ASyncAndroid/MainActivity.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Java.Interop;
 
 namespace ASyncAndroid
 {
@@ -22,35 +23,94 @@ namespace ASyncAndroid
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+        }
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
-            
-            button.Click += async delegate
+        [Export]
+        public void GenDBClicked(View v)
+        {
+            var tf1 = FindViewById<EditText>(Resource.Id.editText1);
+            var size = int.Parse(tf1.Text);
+
+            Console.WriteLine("Gen db");
+            GenClientData(size);
+            Console.WriteLine("Done gen db");
+        }
+
+        [Export]
+        public void CheckDbClicked(View v)
+        {
+            Console.WriteLine("Check db");
+            var tf1 = FindViewById<EditText>(Resource.Id.editText1);
+            var tf2 = FindViewById<EditText>(Resource.Id.editText2);
+
+            var size = int.Parse(tf1.Text);
+            var changedPer = int.Parse(tf2.Text);
+            CheckDbData(size, changedPer);
+            Console.WriteLine("Done checking db");
+        }
+
+        [Export]
+        public void GenBFClicked(View v)
+        {
+            Console.WriteLine("Gen BF");
+            GenBfFileFromDb();
+            Console.WriteLine("Done gen bf");
+        }
+
+        [Export]
+        public async void DownloadClicked(View v)
+        {
+            Console.WriteLine("Download clicked", ((Button)v).Text);
+            var tf1 = FindViewById<EditText>(Resource.Id.editText1);
+            var tf2 = FindViewById<EditText>(Resource.Id.editText2);
+
+            var serverFile = tf1.Text;
+            var localFile = tf2.Text;
+
+            await NetworkManager.FtpDownload("ftp://10.81.4.120/" + serverFile, localFile);
+
+            Console.WriteLine("Done downloading");
+        }
+
+        [Export]
+        public void Patch1Clicked(View v)
+        {
+            Console.WriteLine("Patch 1 and gen ibf");
+            PatchAndGenIBFFile();
+            Console.WriteLine("Done generating ibf file");
+        }
+
+        [Export]
+        public async void UploadClicked(View v)
+        {
+            Console.WriteLine("Upload clicked");
+
+            var tf1 = FindViewById<EditText>(Resource.Id.editText1);
+            var fileName = tf1.Text;
+            var docFolder = DbManager.AppDir;
+            using (var file = File.OpenRead(Path.Combine(docFolder, fileName)))
             {
-                Console.WriteLine("Starting...");
-                //var d = Android.OS.Environment.ExternalStorageDirectory + "/async/db";
-                //Console.WriteLine(d);
+                await NetworkManager.FtpUpload("ftp://10.81.4.120", file, fileName);
+            }
 
-                //GenClientData(5000000);
-                //CheckDbData(5000000);
-                //Console.WriteLine("Done generating client db");
-                //GenBfFileFromDb();
-//                await DownloadPatch1File();
-//                Console.WriteLine("Done downloading patch1 file");
+            Console.WriteLine("Done uploading");
 
-//                PatchAndGenIBFFile();
-//                Console.WriteLine("Donw generating ibf file");
+        }
 
-//                await UploadIBfFileToServer();
-//                Console.WriteLine("Done uploading ibf file");
+        [Export]
+        public void Patch2Clicked(View v)
+        {
+            Console.WriteLine("Patch2 clicked");
+            ClientApplyPatch2();
+            Console.WriteLine("Done patch 2");
+        }
 
-//                await DownloadPatch2File();
-//                Console.WriteLine("done downloading p2 file");
-
-                var a = 0;
-            };
+        [Export]
+        public void DisposeDbClicked(View v)
+        {
+            Console.WriteLine("Dispose db clicked");
+            DbManager.Dispose();
+            Console.WriteLine("Done disposing db");
         }
 
         private static void GenClientData(int size)
@@ -63,14 +123,13 @@ namespace ASyncAndroid
                 }
                 trans.Commit();
             }
-            DbManager.Dispose();
         }
 
-        private static bool CheckDbData(int size)
+        private static bool CheckDbData(int size, int changedPercent)
         {
             using (var trans = DbManager.Engine.GetTransaction())
             {
-                foreach (var item in DataGen.Gen(size, 0))
+                foreach (var item in DataGen.Gen(size, changedPercent))
                 {
                     var inDbVal = trans.Select<string, string>("t1", item.Key);
                     if (!inDbVal.Exists)
@@ -83,7 +142,6 @@ namespace ASyncAndroid
                     }
                 }
             }
-            DbManager.Dispose();
             return true;
         }
 
@@ -92,27 +150,12 @@ namespace ASyncAndroid
             using (var trans = DbManager.Engine.GetTransaction())
             {
                 var clientDic = trans.SelectForward<string, string>("t1").Select(t => new KeyValuePair<string, string>(t.Key, t.Value));
-                var docFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+                var docFolder = DbManager.AppDir;
                 using (var bffile = File.OpenWrite(Path.Combine(docFolder, "bffile.dat")))
                 {
                     KeyValSync.ClientGenBfFile(clientDic, (int)trans.Count("t1"), bffile);
                 }
             }
-            DbManager.Dispose();
-        }
-
-        private static async Task UploadBfFileToServer()
-        {
-            var docFolder = DbManager.AppDir;
-            using (var bffile = File.OpenRead(Path.Combine(docFolder, "bffile.dat")))
-            {
-                await NetworkManager.FtpUpload("ftp://10.81.4.120", bffile, "bffile.dat");
-            }
-        }
-
-        private static async Task DownloadPatch1File()
-        {
-            await NetworkManager.FtpDownload("ftp://10.81.4.120/patch1filetext.dat", "patch1file.dat");
         }
 
         private static void PatchAndGenIBFFile()
@@ -138,20 +181,18 @@ namespace ASyncAndroid
                 }
                 trans.Commit();
             }
-            DbManager.Dispose();
         }
 
-        private static async Task DownloadPatch2File()
+        static void ClientApplyPatch2()
         {
-            await NetworkManager.FtpDownload("ftp://10.81.4.120/patch2file.dat", "patch2file.dat");
-        }
-
-        private static async Task UploadIBfFileToServer()
-        {
-            var docFolder = DbManager.AppDir;
-            using (var bffile = File.OpenRead(Path.Combine(docFolder, "ibffile.dat")))
+            using (var trans = DbManager.Engine.GetTransaction())
             {
-                await NetworkManager.FtpUpload("ftp://10.81.4.120", bffile, "ibffile.dat");
+                var docFolder = DbManager.AppDir;
+                using (var patch2File = File.OpenRead(Path.Combine(docFolder, "patch2file.dat")))
+                {
+                    KeyValSync.ClientPatch<string, string>(currItem => trans.Insert("t1", currItem.Key, currItem.Value), patch2File);
+                }
+                trans.Commit();
             }
         }
     }
--- ASyncAndroid/Resources/Resource.designer.cs
+++ ASyncAndroid/Resources/Resource.designer.cs
@@ -60,8 +60,35 @@ namespace ASyncAndroid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050009
+			public const int button1 = 2131034121;
+			
+			// aapt resource value: 0x7f050003
+			public const int checkBtn = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int downloadBtn = 2131034117;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int editText1 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int editText2 = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int genBloomBtn = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int myButton = 2131034114;
+			
+			// aapt resource value: 0x7f050006
+			public const int patch1Btn = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int patch2Btn = 2131034120;
+			
+			// aapt resource value: 0x7f050007
+			public const int uploadBtn = 2131034119;
 			
 			static Id()
 			{
