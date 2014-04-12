7a4ba21a8076322d26d1328b6793d1c7da753181
Sinh Pham <phamansinh@gmail.com>
2014-04-12 16:02:41 -0600

    StrataEstimatorTest

--- ASyncLib/StrataEstimator.cs
+++ ASyncLib/StrataEstimator.cs
@@ -6,40 +6,34 @@ using System.Threading.Tasks;
 
 namespace ASyncLib
 {
-    class StrataEstimator
+    public class StrataEstimator
     {
         public StrataEstimator()
         {
             _ibfList = new List<IBF>(32);
             for (var i = 0; i < 32; ++i)
             {
-                _ibfList.Add(new IBF());
+                _ibfList.Add(new IBF(80, BloomFilter.DefaultHashFuncs(3)));
             }
-
-            _hzFunc = new MurmurHash3_x86_32();
         }
         List<IBF> _ibfList;
-        IHashFunc _hzFunc;
 
         public void Encode<TKey, TValue>(Dictionary<TKey, TValue> dic)
         {
             foreach (var item in dic)
             {
-                var block = item.Key + "-" + item.Value;
-                var bBlock = Helper.GetBytes(block);
+                var id = KeyValSync.KeyValToId(item);
 
-                var val = BitConverter.ToInt32(_hzFunc.ComputeHash(bBlock), 0);
+                var i = NumTrailingBinaryZeros(id);
 
-                var i = NumTrailingBinaryZeros(val);
-
-                _ibfList[i].Add(val);
+                _ibfList[i].Add(id);
             }
         }
 
-        public int Decode()
+        public int Estimate()
         {
             var count = 0;
-            for (var i = 32; i >= -1; --i)
+            for (var i = _ibfList.Count - 1; i >= -1; --i)
             {
                 var amb = new List<long>();
                 var bma = new List<long>();
@@ -62,12 +56,10 @@ namespace ASyncLib
             return ret;
         }
 
-
-
-        public static int NumTrailingBinaryZeros(int n)
+        public static int NumTrailingBinaryZeros(long n)
         {
             var mask = 1;
-            for (var i = 0; i < 32; i++, mask <<= 1)
+            for (var i = 0; i < 64; i++, mask <<= 1)
             {
                 if ((n & mask) != 0)
                 {
@@ -75,7 +67,7 @@ namespace ASyncLib
                 }
             }
 
-            return 32;
+            return 64;
         }
     }
 }
---
+++ AsyncTest/StrataEstimatorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ASyncLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AsyncTest
+{
+    [TestClass]
+    public class StrataEstimatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var est = new StrataEstimator();
+
+            var _clientDic = new Dictionary<string, string>();
+            var _serverDic = new Dictionary<string, string>();
+            for (var i = 0; i < 2000; ++i)
+            {
+                _clientDic.Add(i.ToString(), i.ToString());
+            }
+            for (var i = 0; i < 2005; ++i)
+            {
+                _serverDic.Add(i.ToString(), i.ToString());
+            }
+
+            est.Encode(_clientDic);
+
+            var serverEst = new StrataEstimator();
+            serverEst.Encode(_serverDic);
+
+            var diff = est - serverEst;
+
+            var n = diff.Estimate();
+            Assert.IsTrue(true);
+        }
+    }
+}
